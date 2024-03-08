101b775f92f408f1f1ed61a3d30fc6d27825e759
Nikita Moskalev <nikmosyl@gmail.com>
2024-03-08 16:01:58 +0400

    Initial Commit

---
+++ StateDataFlow/StateDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateDataFlowApp.swift
+//  StateDataFlow
+//
+//  Created by nikita on 08.03.24.
+//
+
+import SwiftUI
+
+@main
+struct StateDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
